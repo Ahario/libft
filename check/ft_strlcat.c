8b7db8438b196431e6db88d87f49c9ba32f12ca1
Ahario <81345604+Ahario@users.noreply.github.com>
2021-11-21 22:09:06 +0900

    Add files via upload

--- check/ft_strlcat.c
+++ check/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: hyeo <hyeo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 12:12:11 by hyeo              #+#    #+#             */
-/*   Updated: 2021/11/19 16:02:13 by hyeo             ###   ########.fr       */
+/*   Updated: 2021/11/20 16:40:41 by hyeo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,13 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	i = 0;
 	srclen = ft_strlen(src);
 	destlen = ft_strlen(dst);
-	if (srclen == 0)
-		return (dstsize);
-	dst += destlen;
-	while (*src != '\0' && (destlen + i + 1) < dstsize)
+	if (dstsize < destlen)
+		return (dstsize + srclen);
+	while (src[i] != '\0' && (destlen + i + 1) < dstsize)
 	{
-		*dst = *src;
-		dst++;
-		src++;
+		dst[destlen + i] = src[i];
 		i++;
 	}
-	*dst = '\0';
-	if (dstsize < destlen)
-		return (srclen + dstsize);
+	dst[destlen + i] = '\0';
 	return (srclen + destlen);
 }

10b613217a64367f62161ca3eecfb7af10cbf3ee
Ahario <81345604+Ahario@users.noreply.github.com>
2021-12-03 12:29:40 +0900

    Add files via upload

---
+++ finalfinal/ft_memset.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyeo <hyeo@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/16 13:27:53 by hyeo              #+#    #+#             */
+/*   Updated: 2021/11/19 13:07:34 by hyeo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t			i;
+	unsigned char	*dest;
+
+	i = 0;
+	dest = (unsigned char *)b;
+	while (i != len)
+	{
+		dest[i] = (unsigned char)c;
+		i++;
+	}
+	return (dest);
+}
+//int main()
+//{
+//	unsigned char b[10] = "abcdefghi";
+//	int c = 41;
+//	size_t len = 5;
+//	printf("%s", ft_memset(b, c, len));
+//}
